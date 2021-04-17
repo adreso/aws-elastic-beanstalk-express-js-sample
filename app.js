945917d32356fc7421f155e9010ff53f609d6d09
adreso <arrieta10andres@gmail.com>
2021-04-16 20:32:32 -0500

    Cambios console

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+console.log('hey esta vaina sirve?');
+
 app.get('/', (req, res) => res.send('Hello World andrew!'));
 
 app.listen(port);
